eb3fb765a442d6bdf7af0872f4f859420579dac9
Danikopaniko <c.tar.daniiel@student,uzhnu.edu.ua>
2026-06-09 11:52:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab7_DelegatesAndEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            Action<int> printAction = num => Console.Write(num + " ");
+
+            numbers.ForEach(printAction);
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
